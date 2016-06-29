8b630658ad50dbdbc21b6437639a26d49e73860e
chao-ji <jic@indiana.edu>
2016-06-29 00:52:01 -0400

    Update NaiveBayesTrain.java

--- NaiveBayesTrain/NaiveBayesTrain.java
+++ NaiveBayesTrain/NaiveBayesTrain.java
@@ -18,81 +18,62 @@ import org.apache.hadoop.util.GenericOptionsParser;
 
 public class NaiveBayesTrain
 {
-	public static class MeanMapper extends Mapper<Object, Text, Text, SumCountTuple>
+	public static class MeanMapper extends Mapper<Object, Text, Text, MeanCountTuple>
 	{
-		private final static IntWritable one = new IntWritable(1);
-
 		public void map(Object key, Text value, Context context) throws IOException, InterruptedException
 		{
 			String line = value.toString();
-			String[] parts = line.split("\t");
+			String[] tsv = line.split("\t");
 
-			String label = parts[parts.length - 1];
-			for (int i = 0; i < parts.length - 1; i++)
+			String classID = tsv[tsv.length - 1];
+			for (int i = 0; i < tsv.length - 1; i++)
 			{
-				SumCountTuple tuple = new SumCountTuple();
-				
+				String featureID = Integer.toString(i);
+				MeanCountTuple tuple = new MeanCountTuple();
 				Text outputKey = new Text();
-				outputKey.set(Integer.toString(i) + "_" + label);
 
-				tuple.setSum(Double.parseDouble(parts[i]));
-				tuple.setCount(one.get());
+				outputKey.set(featureID + ":" + classID);
+				tuple.setMean(Double.parseDouble(tsv[i]));
+				tuple.setCount(1);
 				context.write(outputKey, tuple);
 			}
 		}
 	}
 
-	public static class MeanCombiner extends Reducer<Text, SumCountTuple, Text, SumCountTuple>
+	public static class MeanReducer extends Reducer<Text, MeanCountTuple, Text, MeanCountTuple>
 	{
-		private SumCountTuple tuple = new SumCountTuple();
+		private MeanCountTuple result = new MeanCountTuple();
 
-		public void reduce(Text key, Iterable<SumCountTuple> values, Context context) throws IOException, InterruptedException
+		public void reduce(Text key, Iterable<MeanCountTuple> values, Context context) throws IOException, InterruptedException
 		{
-			double sum = 0.0;
 			int count = 0;
-			for (SumCountTuple val : values)
+			double sum = 0.0;
+			for (MeanCountTuple val : values)
 			{
-				sum += val.getSum();
+				sum += val.getMean() * val.getCount();
 				count += val.getCount();
 			}
-			tuple.setSum(sum);
-			tuple.setCount(count);
-			context.write(key, tuple);
-		}
-	}
-
-	public static class MeanReducer extends Reducer<Text, SumCountTuple, Text, Text>
-	{
-		Text result = new Text();
-		public void reduce(Text key, Iterable<SumCountTuple> values, Context context) throws IOException, InterruptedException
-		{
-			double sum = 0.0;
-			int count = 0;
-			double mean = 0.0;
-			for (SumCountTuple val : values)
-			{
-				sum += val.getSum();
-				count += val.getCount();
-			}
-			mean = sum / count;
-			result.set(Double.toString(mean) + ":" + Integer.toString(count));
+			result.setMean(sum / count);
+			result.setCount(count);
 			context.write(key, result);
 		}
 	}
+
 
 	public static class SumSquareMapper extends Mapper<Object, Text, Text, DoubleWritable>
 	{
 		public void map(Object key, Text value, Context context) throws IOException, InterruptedException
 		{
 			String line = value.toString();
-			String[] parts = line.split("\t");
+			String[] tsv = line.split("\t");
 
-			String label = parts[parts.length - 1];
-			for (int i = 0; i < parts.length - 1; i++)
+			String classID = tsv[tsv.length - 1];
+			for (int i = 0; i < tsv.length - 1; i++)
 			{
+				String featureID = Integer.toString(i);
 				Text outputKey = new Text();
-				outputKey.set(Integer.toString(i) + "_" + label);
-				double val = Double.parseDouble(parts[i]);
+				outputKey.set(featureID + ":" + classID);
+				double val = Double.parseDouble(tsv[i]);
 				context.write(outputKey, new DoubleWritable(val * val));
 			}
 		}
@@ -109,13 +90,21 @@ public class NaiveBayesTrain
 		}
 	}
 
+
 	public static class VarianceMapper extends Mapper<Object, Text, Text, Text>
 	{
 		public void map(Object key, Text value, Context context) throws IOException, InterruptedException
 		{
 			String line = value.toString();
-			String[] parts = line.split("\t");
-			context.write(new Text(parts[0]), new Text(parts[1]));
+			String[] tsv = line.split("\t");
+			Text outputKey = new Text(tsv[0]);
+			Text outputVal = new Text();
+			if (tsv.length == 2)
+				outputVal.set(tsv[1]);
+			else if (tsv.length == 3)
+				outputVal.set(tsv[1] + "\t" + tsv[2]);
+
+			context.write(outputKey, outputVal);
 		}
 	}
 
@@ -129,16 +118,15 @@ public class NaiveBayesTrain
 			double sumsquare = 0.0;
 			double var = 0.0;
 
-			for (Text value : values)
+			for (Text val : values)
 			{
-				String content = value.toString();
-				String[] parts = content.split(":");
-				if (parts.length == 1)
-					sumsquare = Double.parseDouble(parts[0]);
+				String[] tsv = val.toString().split("\t");
+				if (tsv.length == 1)
+					sumsquare = Double.parseDouble(tsv[0]);
 				else
 				{
-					mean = Double.parseDouble(parts[0]);
-					count = Integer.parseInt(parts[1]);
+					mean = Double.parseDouble(tsv[0]);
+					count = Integer.parseInt(tsv[1]);
 				}
 			}
 
@@ -153,15 +141,14 @@ public class NaiveBayesTrain
 		public void map(Object key, Text value, Context context) throws IOException, InterruptedException
 		{
 			String line = value.toString();
-			String[] parts = line.split("\t");
-			String[] list1 = parts[0].split("_");
-			String[] list2 = parts[1].split(":");
+			String[] tsv = line.split("\t");
+			String[] list = tsv[0].split(":");
 
-			if (list1[0].equals("0"))
+			if (list[0].equals("0"))
 			{
-				String label = list1[1];
-				String count = list2[1];
-				context.write(new Text("key"), new Text(label + ":" + count));
+				String classID = list[1];
+				String count = tsv[2];
+				context.write(new Text("key"), new Text(classID + ":" + count));
 			}
 		}
 	}
@@ -172,20 +159,20 @@ public class NaiveBayesTrain
 		{
 			int total = 0;
 			List<Integer> counts = new ArrayList<Integer>();
-			List<String> labels = new ArrayList<String>();
+			List<String> classID = new ArrayList<String>();
 
 			for (Text val : values)
 			{
-				String[] parts = val.toString().split(":");
-				total += Integer.parseInt(parts[1]);
-				counts.add(Integer.parseInt(parts[1]));
-				labels.add(parts[0]);
+				String[] tsv = val.toString().split(":");
+				total += Integer.parseInt(tsv[1]);
+				counts.add(Integer.parseInt(tsv[1]));
+				classID.add(tsv[0]);
 			}
 
 			for (int i = 0; i < counts.size(); i++)
 			{
-				double outputValue = (double) counts.get(i) / total;
-				context.write(new Text(labels.get(i)), new DoubleWritable(outputValue));
+				double outputVal = (double) counts.get(i) / total;
+				context.write(new Text(classID.get(i)), new DoubleWritable(outputVal));
 			}	
 		}
 	}
@@ -204,14 +191,10 @@ public class NaiveBayesTrain
 		Job job = new Job(conf, "Mean");
 		job.setJarByClass(NaiveBayesTrain.class);
 		job.setMapperClass(MeanMapper.class);
-		job.setCombinerClass(MeanCombiner.class);
+		job.setCombinerClass(MeanReducer.class);
 		job.setReducerClass(MeanReducer.class);
-
-		job.setMapOutputKeyClass(Text.class);
-		job.setMapOutputValueClass(SumCountTuple.class);
-
 		job.setOutputKeyClass(Text.class);
-		job.setOutputValueClass(Text.class);
+		job.setOutputValueClass(MeanCountTuple.class);
 		FileInputFormat.addInputPath(job, new Path(otherArgs[0]));
 		FileOutputFormat.setOutputPath(job, new Path(otherArgs[1] + "_mean"));
 		job.waitForCompletion(true);
@@ -222,7 +205,6 @@ public class NaiveBayesTrain
 		job.setMapperClass(SumSquareMapper.class);
 		job.setCombinerClass(SumSquareReducer.class);
 		job.setReducerClass(SumSquareReducer.class);
-
 		job.setOutputKeyClass(Text.class);
 		job.setOutputValueClass(DoubleWritable.class);
 		FileInputFormat.addInputPath(job, new Path(otherArgs[0]));
@@ -234,7 +216,6 @@ public class NaiveBayesTrain
 		job.setJarByClass(NaiveBayesTrain.class);
 		job.setMapperClass(VarianceMapper.class);
 		job.setReducerClass(VarianceReducer.class);
-
 		job.setOutputKeyClass(Text.class);
 		job.setOutputValueClass(Text.class);
 		FileInputFormat.addInputPath(job, new Path(otherArgs[1] + "_mean"));
@@ -242,7 +223,8 @@ public class NaiveBayesTrain
 		FileOutputFormat.setOutputPath(job, new Path(otherArgs[1] + "_var"));
 		job.waitForCompletion(true);
 
-    // Configure job that computes class prior
+
+        // Configure job that computes class prior
 		job = new Job(conf, "Prior");
 		job.setJarByClass(NaiveBayesTrain.class);
 		job.setMapperClass(PriorMapper.class);
@@ -255,33 +237,37 @@ public class NaiveBayesTrain
 		job.setOutputValueClass(DoubleWritable.class);
 		FileInputFormat.addInputPath(job, new Path(otherArgs[1] + "_mean"));
 		FileOutputFormat.setOutputPath(job, new Path(otherArgs[1] + "_prior"));
-
 		System.exit(job.waitForCompletion(true) ? 0 : 1);
 	}
 
-	public static class SumCountTuple implements Writable
+	public static class MeanCountTuple implements Writable
 	{
-		private double sum = 0.0;
+		private double mean = 0.0;
 		private int count = 0;
 
-		public Double getSum() { return sum; }
+		public double getMean() { return mean; }
 
-		public void setSum(Double sum) { this.sum = sum; }
+		public void setMean(double mean) { this.mean = mean; }
 
-		public Integer getCount() { return count; }
+		public int getCount() { return count; }
 
-		public void setCount(Integer count) { this.count = count; }
+		public void setCount(int count) { this.count = count; }
 
 		public void readFields(DataInput in) throws IOException
 		{
-			sum = in.readDouble();
+			mean = in.readDouble();
 			count = in.readInt();
 		}
 
 		public void write(DataOutput out) throws IOException
 		{
-			out.writeDouble(sum);
+			out.writeDouble(mean);
 			out.writeInt(count);
+		}
+
+		public String toString()
+		{
+			return Double.toString(mean) + "\t" + Integer.toString(count);
 		}
 	}
 }
